385d93a064feefab0d4e232154e73b72ae4708cd
Bethany Leap <bethanyjleap@gmail.com>
2022-03-26 20:13:13 -0400

    initiated script of helper functions

---
+++ R/helpers.R
@@ -0,0 +1,14 @@
+#' @title Save a Marvel API key to the \code{.Renviron} for future use. 
+#' 
+#' @description This function allows the user to save their Marvel API key securely in their 
+#' \code{.Renviron} file. Only run this in the console, do not save in your script. 
+#' 
+#' @param key A string containing the 
+#' 
+#' 
+
+marvel_api_key <- function(key, overwrite = FALSE, create = FALSE) {
+  # checks here
+  # code here
+  # see {tidycensus} helpers.R documentation for further direction
+}
