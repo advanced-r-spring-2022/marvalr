b56f78ebce4bdc2679fca9f09f7510e8aab7d7e8
Dashiell Nusbaum <dashiellnusbaum@gmail.com>
2022-04-24 21:53:55 -0400

    added private api key spot to function, works

--- R/marvel_api_keys.R
+++ R/marvel_api_keys.R
@@ -16,6 +16,7 @@
 #' Uses the {tidycensus} package, particularly the census_api_key() function, as guidance for creating this function
 
 marvel_api_keys <- function (public_api_key,
+                             private_api_key,
                              overwrite = FALSE,
                              install = FALSE) {
   if (install) {
@@ -31,26 +32,36 @@ marvel_api_keys <- function (public_api_key,
       if (isTRUE(overwrite)) {
         message("Your original .Renviron will be backed up and stored in your R HOME directory if needed.")
         oldenv = read.table(renv, stringsAsFactors = FALSE)
-        newenv <- oldenv[-grep("MARVEL_PUBLIC_API_KEY", 
+        newenv <- oldenv[-grep("MARVEL_PUBLIC_API_KEY", # newenv table is oldenv table minus lines that include MARVEL_PUBLIC_API_KEY or MARVEL_PRIVATE_API_KEY
+                               "MARVEL_PRIVATE_API_KEY",
                                oldenv), ]
         write.table(newenv, renv, quote = FALSE, sep = "\n", 
                     col.names = FALSE, row.names = FALSE)
       }
       else {
         tv <- readLines(renv)
-        if (any(grepl("MARVEL_PUBLIC_API_KEY", tv))) {
+        if (any(grepl("MARVEL_PUBLIC_API_KEY", tv)) & any(grepl("MARVEL_PRIVATE_API_KEY", tv))) {
+          stop("A MARVEL_PUBLIC_API_KEY and a MARVEL_PRIVATE_API_KEY already exist. You can overwrite them with the argument overwrite=TRUE", 
+               call. = FALSE)
+        } else if (any(grepl("MARVEL_PUBLIC_API_KEY", tv))) {
           stop("A MARVEL_PUBLIC_API_KEY already exists. You can overwrite it with the argument overwrite=TRUE", 
+               call. = FALSE)
+        } else if (any(grepl("MARVEL_PRIVATE_API_KEY", tv))) {
+          stop("A MARVEL_PRIVATE_API_KEY already exists. You can overwrite it with the argument overwrite=TRUE", 
                call. = FALSE)
         }
       }
     }
     public_api_keyconcat <- paste0("MARVEL_PUBLIC_API_KEY='", public_api_key, "'")
+    private_api_keyconcat <- paste0("MARVEL_PRIVATE_API_KEY='", private_api_key, "'")
     write(public_api_keyconcat, renv, sep = "\n", append = TRUE)
-    message("Your API key has been stored in your .Renviron and can be accessed by Sys.getenv(\"MARVEL_PUBLIC_API_KEY\"). \nTo use now, restart R or run `readRenviron(\"~/.Renviron\")`")
+    write(private_api_keyconcat, renv, sep = "\n", append = TRUE)
+    message("Your API keys have been stored in your .Renviron and can be accessed by Sys.getenv(\"MARVEL_PUBLIC_API_KEY\") and Sys.getenv(\"MARVEL_PRIVATE_API_KEY\"). \nTo use now, restart R or run `readRenviron(\"~/.Renviron\")`")
     return(public_api_key)
   }
   else {
-    message("To install your API key for use in future sessions, run this function with `install = TRUE`.")
+    message("To install your API keys for use in future sessions, run this function with `install = TRUE`.")
     Sys.setenv(MARVEL_PUBLIC_API_KEY = public_api_key)
+    Sys.setenv(MARVEL_PRIVATE_API_KEY = private_api_key)
   }
 }
