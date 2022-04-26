d5e67ffffba9fea370eb293c59d454f4ab3f107e
Bethany Leap <69605884+bethanyleap@users.noreply.github.com>
2022-04-26 18:34:16 -0400

    Bethleap (#2)
    * initiated script of helper functions
    
    * added api connector
    
    * fixed hashing algorithm bug that prevented API connection
    
    * make data
    
    * helper function for accessing API
    
    * update helper function
    
    Co-authored-by: Bethany Leap <bethanyjleap@gmail.com>

--- R/helpers.R
+++ R/helpers.R
@@ -1,30 +1,15 @@
-#' @title Save a Marvel API key to the \code{.Renviron} for future use. 
-#' 
-#' @description This function allows the user to save their Marvel API key securely in their 
-#' \code{.Renviron} file. Only run this in the console, do not save in your script. 
-#' 
-#' @param key A string containing the 
-#' 
-#' @author Bethany Leap
-#' 
-
-marvel_api_key <- function(key, overwrite = FALSE, create = FALSE) {
-  # checks here
-  # code here
-  # see {tidycensus} helpers.R documentation for further direction
-}
-
-
 #' @title Access the base point for the Marvel API
 #' 
 #' @description This is the base function for accessing the Marvel API base point. It is the foundation of all functions in the package.
 #' 
 #' @param path the end point
 #' 
+#' @param public_key the user's public API key
+#' 
+#' @param private_key the user's private API key
+#' 
 #' @author Bethany Leap
 #' 
-#' 
-#' @noRd # make this internal function
 
 marvel_api <- function(path, public_key, private_key) {
   
