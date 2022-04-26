946011b59eaa7906bd15a994f19617ef0e9f4c82
Bethany Leap <bethanyjleap@gmail.com>
2022-04-26 18:16:59 -0400

    helper function for accessing API

--- R/helpers.R
+++ R/helpers.R
@@ -1,20 +1,3 @@
-#' @title Save a Marvel API key to the \code{.Renviron} for future use. 
-#' 
-#' @description This function allows the user to save their Marvel API key securely in their 
-#' \code{.Renviron} file. Only run this in the console, do not save in your script. 
-#' 
-#' @param key A string containing the 
-#' 
-#' @author Bethany Leap
-#' 
-
-marvel_api_key <- function(key, overwrite = FALSE, create = FALSE) {
-  # checks here
-  # code here
-  # see {tidycensus} helpers.R documentation for further direction
-}
-
-
 #' @title Access the base point for the Marvel API
 #' 
 #' @description This is the base function for accessing the Marvel API base point. It is the foundation of all functions in the package.
