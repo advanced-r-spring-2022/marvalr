204dfd17f06ae4fb521338b794bd981c5804a975
danielebp94@gmail.com <73570354+dbernal1@users.noreply.github.com>
2022-05-01 22:37:13 -0400

    get_events

---
+++ R/get_events.R
@@ -0,0 +1,115 @@
+#' @title Get tibble of creators information from Marvel API
+#' 
+#' @description Get tibble containing information related to creators through the Marvel API based on user input criteria. 
+#' 
+#' @param limit An integer. Limit the result set to the specified number of resources (max 100).
+#' @param offset An integer. Skip the specified number of resources in the result set.
+#' @param nameStartsWith A character. Return characters with names that begin with the specified string (e.g. Sp).
+#' @param comic An integer. The comic ID (e.g., 101094).
+#' @param event An integer. The event ID (e.g., 305).
+#' @param series An integer. The series ID (e.g., 25990).
+#' @param story An integer. The story ID (e.g., 223124).
+#' 
+#' @return A dataframe of up to 100 characters containing their ID, name, description, time last modified, resource URI, and nested lists of thumbnails, comics, series, stories, events, and urls.
+#' 
+#' @author Bethany Leap, Daniel Bernal Panqueva, Dashiell Nusbaum, Ian M Davis
+#' 
+#' @examples
+#' 
+
+
+get_events <- function(limit = 100, # default limit should be 100
+                           offset = 0, # how many results to offset by
+                           comic = NULL, # default comicID should be NULL
+                           creator = NULL, # default creatorID should be NULL
+                           series = NULL, # default seriesID should be NULL
+                           story = NULL) { # default storyID should be NULL
+  
+  marvel_public_api_key <- Sys.getenv("MARVEL_PUBLIC_API_KEY") # get public key
+  marvel_private_api_key <- Sys.getenv("MARVEL_PRIVATE_API_KEY") # get priv key
+  
+  # should only be enter one of c("comicID", "eventID", "seriesID", "storyID")
+  # so number of nulls should be 3 (if 1 entered) or 4 (if none entered)
+  
+  if(sum(is.null(comic),
+         is.null(creator),
+         is.null(series),
+         is.null(story)) <3) {
+    stop("Please choose to enter an argument for only one of comic, creator, series, or story.")
+  }
+  
+  # entries for arguments should be characters/strings
+  
+  if(!is.null(comic) & !is.character(comic)) {
+    stop("Comic must be entered as string/characters. Ex. \"Carnage #2\"")
+  }
+  
+  if(!is.null(creator) & !is.character(creator)) {
+    stop("Creator must be entered as string/characters. Ex. \"Peter David\"")
+  }
+  
+  if(!is.null(series) & !is.character(series)) {
+    stop("Series must be entered as string/characters. Ex. \"Eternals\"")
+  }
+  
+  if(!is.null(story) & !is.character(story)) {
+    stop("Story must be entered as string/characters.") # still need example, not sure what a story?
+  }
+  
+  
+  # timestamp, hash, and url
+  ts <- round(as.numeric(Sys.time())*1000) 
+  to_hash <- sprintf("%s%s%s",
+                     ts,
+                     marvel_private_api_key,
+                     marvel_public_api_key)
+  hash <- digest::digest(to_hash, "md5", FALSE)
+  
+  base_url <- "https://gateway.marvel.com/v1/public/" # base url
+  
+  if (!is.null(comic)) { # if user enters comicsID
+    comicsID = comics_id$id[comics_id$title == comic]
+    events_base_url <- paste0(base_url, "comics/", comicsID, "/events")
+    
+  } else if (!is.null(creator)) { # if user enters creatorID
+    
+    creatorID = creators_id$id[creators_id$fullName == creator]
+    events_base_url <- paste0(base_url, "creators/", creatorID, "/events")
+    
+  } else if (!is.null(series)) { # if user enters seriesID
+    
+    seriesID = series_id$id[series_id$title == series]
+    events_base_url  <- paste0(base_url, "series/", seriesID, "/events")
+    
+  } else if (!is.null(story)) { # if user enters storyID
+    
+    storyID = stories_id$id[stories_id$tile == story]
+    events_base_url <- paste0(base_url, "stories/", storyID, "/events")
+    
+  } else { # if user doesnt enter any ids
+    
+    events_base_url <- paste0(base_url, "events")
+  }
+  
+  httr::modify_url(url = events_base_url, # take base url
+                   query = list(limit = limit,
+                                offset = offset,
+                                ts = ts, # timestamp
+                                hash = hash, # hash
+                                apikey = marvel_public_api_key)) -> # public api key
+    events_url
+  
+  GET_events <- httr::GET(events_url) # GET the url
+  stopifnot(httr::status_code(GET_events) == 200) # stop if error code
+  # turn into a tidy dataframe
+  content_events <- httr::content(GET_events) # retrieve contents of req
+  tibble_events <- tibble::tibble(content_events) # turn into tibble
+  unnest1_events <- tidyr::unnest(data = tibble_events[7,1],
+                                      cols = content_events) # unnest
+  unnest2_events <- tidyr::unnest(unnest1_events[5,1],
+                                      cols = content_events) # unnest
+  unnest3_events <- tidyr::unnest_wider(data = unnest2_events,
+                                            col = content_events) # gets df
+  
+  return(unnest3_events) # return dataframe to user
+}
