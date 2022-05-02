421fdc0fef43dbcb9c0cb7cdbdb9eafbb0220757
danielebp94@gmail.com <73570354+dbernal1@users.noreply.github.com>
2022-05-02 00:17:11 -0400

    get_events updated

--- R/get_events.R
+++ R/get_events.R
@@ -1,21 +1,28 @@
-#' @title Get tibble of creators information from Marvel API
+#' @title Get tibble of events information from Marvel API
 #' 
-#' @description Get tibble containing information related to creators through the Marvel API based on user input criteria. 
+#' @description Get tibble containing information related to events through the Marvel API based on user input criteria.
 #' 
-#' @param limit An integer. Limit the result set to the specified number of resources (max 100).
+#' @param limit An integer. Limit the result set to the specified number of resources (defaulted to 100).
 #' @param offset An integer. Skip the specified number of resources in the result set.
-#' @param nameStartsWith A character. Return characters with names that begin with the specified string (e.g. Sp).
-#' @param comic An integer. The comic ID (e.g., 101094).
-#' @param event An integer. The event ID (e.g., 305).
-#' @param series An integer. The series ID (e.g., 25990).
-#' @param story An integer. The story ID (e.g., 223124).
+#' @param comic A character vector. A Marvel comic issue, collection, graphic novel, or digital comic.
+#' @param creator A character vector. A person or entity that makes comics.
+#' @param series A character vector. Sequentially numbered list of comics with the same title and volume.
+#' @param character A character vector. Full name of the Marvel character.
+#' @param story A character vector. Indivisible components of comics.
 #' 
-#' @return A dataframe of up to 100 characters containing their ID, name, description, time last modified, resource URI, and nested lists of thumbnails, comics, series, stories, events, and urls.
+#' 
 #' 
 #' @author Bethany Leap, Daniel Bernal Panqueva, Dashiell Nusbaum, Ian M Davis
 #' 
-#' @examples
+#' @export get_events
 #' 
+#' @examples 
+#' get_events()
+#'                 
+#' get_events(creator = "Brian Michael Bendis")
+#' 
+#' get_events(character = "Black Widow")
+
 
 
 get_events <- function(limit = 100, # default limit should be 100
@@ -23,37 +30,43 @@ get_events <- function(limit = 100, # default limit should be 100
                            comic = NULL, # default comicID should be NULL
                            creator = NULL, # default creatorID should be NULL
                            series = NULL, # default seriesID should be NULL
+                           character = NULL, # default characterID should be NULL 
                            story = NULL) { # default storyID should be NULL
   
   marvel_public_api_key <- Sys.getenv("MARVEL_PUBLIC_API_KEY") # get public key
-  marvel_private_api_key <- Sys.getenv("MARVEL_PRIVATE_API_KEY") # get priv key
+  marvel_private_api_key <- Sys.getenv("MARVEL_PRIVATE_API_KEY") # get private key
   
   # should only be enter one of c("comicID", "eventID", "seriesID", "storyID")
-  # so number of nulls should be 3 (if 1 entered) or 4 (if none entered)
+  # so number of nulls should be 4 (if 1 entered) or 5 (if none entered)
   
   if(sum(is.null(comic),
          is.null(creator),
          is.null(series),
-         is.null(story)) <3) {
-    stop("Please choose to enter an argument for only one of comic, creator, series, or story.")
+         is.null(character),
+         is.null(story)) < 4) {
+    stop("Please choose to enter an argument for only one of comic, creator, series, character, or story.")
   }
   
   # entries for arguments should be characters/strings
   
   if(!is.null(comic) & !is.character(comic)) {
-    stop("Comic must be entered as string/characters. Ex. \"Carnage #2\"")
+    stop("Comic must be entered as string/characters. Ex. \"Age of Ultron (2013) #1\"")
   }
   
   if(!is.null(creator) & !is.character(creator)) {
-    stop("Creator must be entered as string/characters. Ex. \"Peter David\"")
+    stop("Creator must be entered as string/characters. Ex. \"Brian Michael Bendis\"")
   }
   
   if(!is.null(series) & !is.character(series)) {
-    stop("Series must be entered as string/characters. Ex. \"Eternals\"")
+    stop("Series must be entered as string/characters. Ex. \"All-New X-Men (2015 - 2017)\"")
+  }
+  
+  if(!is.null(character) & !is.character(character)) {
+    stop("Character must be entered as string/characters. Ex. \"Black Widow\"")
   }
   
   if(!is.null(story) & !is.character(story)) {
-    stop("Story must be entered as string/characters.") # still need example, not sure what a story?
+    stop("StoryID must be entered as string/characters.")
   }
   
   
@@ -67,26 +80,30 @@ get_events <- function(limit = 100, # default limit should be 100
   
   base_url <- "https://gateway.marvel.com/v1/public/" # base url
   
-  if (!is.null(comic)) { # if user enters comicsID
+  if (!is.null(comic)) { # if user enters comics
     comicsID = comics_id$id[comics_id$title == comic]
     events_base_url <- paste0(base_url, "comics/", comicsID, "/events")
     
-  } else if (!is.null(creator)) { # if user enters creatorID
+  } else if (!is.null(creator)) { # if user enters creator
     
     creatorID = creators_id$id[creators_id$fullName == creator]
     events_base_url <- paste0(base_url, "creators/", creatorID, "/events")
     
-  } else if (!is.null(series)) { # if user enters seriesID
+  } else if (!is.null(series)) { # if user enters series
     
     seriesID = series_id$id[series_id$title == series]
     events_base_url  <- paste0(base_url, "series/", seriesID, "/events")
     
+  } else if (!is.null(character)) { # if user enters character
+    
+    characterID = character_ids$id[character_ids$name == character]
+    events_base_url  <- paste0(base_url, "characters/", characterID, "/events")
+    
   } else if (!is.null(story)) { # if user enters storyID
     
-    storyID = stories_id$id[stories_id$tile == story]
-    events_base_url <- paste0(base_url, "stories/", storyID, "/events")
+    events_base_url <- paste0(base_url, "stories/", story, "/events")
     
-  } else { # if user doesnt enter any ids
+  } else { # if user doesn't enter any ids
     
     events_base_url <- paste0(base_url, "events")
   }
@@ -101,8 +118,9 @@ get_events <- function(limit = 100, # default limit should be 100
   
   GET_events <- httr::GET(events_url) # GET the url
   stopifnot(httr::status_code(GET_events) == 200) # stop if error code
-  # turn into a tidy dataframe
-  content_events <- httr::content(GET_events) # retrieve contents of req
+  
+  # turn into a tidy data frame
+  content_events <- httr::content(GET_events) # retrieve contents of request
   tibble_events <- tibble::tibble(content_events) # turn into tibble
   unnest1_events <- tidyr::unnest(data = tibble_events[7,1],
                                       cols = content_events) # unnest
@@ -111,5 +129,5 @@ get_events <- function(limit = 100, # default limit should be 100
   unnest3_events <- tidyr::unnest_wider(data = unnest2_events,
                                             col = content_events) # gets df
   
-  return(unnest3_events) # return dataframe to user
+  return(unnest3_events) # return data frame to user
 }
